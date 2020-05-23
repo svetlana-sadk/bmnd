9d1fd90af68774d14c4138fbb81458ccf107a454
Mia <mialy.info@gmail.com>
2020-05-23 18:12:46 +0200

    Vesrion Mia : page d'accueil, page de connexion et création de compte

--- bmnd/app/src/main/java/com/example/bmnd/MainActivity.java
+++ bmnd/app/src/main/java/com/example/bmnd/MainActivity.java
@@ -1,19 +1,39 @@
 package com.example.bmnd;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ProgressBar mPb;
+    private Button mBtnInscription;
+    private Button mBtnConnect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPb = findViewById(R.id.pb);
+        mBtnInscription = findViewById(R.id.btnInscrire);
+        mBtnConnect = findViewById(R.id.btnConnect);
     }
 
     public void onClick(View view) {
-
+        if(view == mBtnConnect) {
+            Intent connectIntent = new Intent(this, ConnexionActivity.class );
+            startActivity(connectIntent);
+        }
+        else if (view == mBtnInscription)
+        {
+            Intent firstConnectIntent = new Intent(this, FirstConnexionActivity.class );
+            startActivity(firstConnectIntent);
+        }
     }
 }
